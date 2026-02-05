6295671cfc76dad79040df7c0b3328354498f869
Aayan-coder20 <khnaayan07@gmail.com>
2026-02-05 21:14:44 +0530

    Add coder and date information to main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Aayan Khan
+# Date:04-02-2026
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+n = int(input("Enter Number: "))
+if n < 0:
+    print(f"Factorial of {abs(n)} is Not Defined")
+else:
+    f = 1
+    for i in range(1, n + 1):
+        f *= i
+    print(f"Factorial of {n} is {f}")
+
